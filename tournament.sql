bc309b69f327741a897cd5c948afdc37608b2ff3
code-rush <parikh.japan30@gmail.com>
2016-02-05 00:10:46 -0800

    Adding drop commands

--- tournament.sql
+++ tournament.sql
@@ -7,13 +7,17 @@
 -- these lines here.
 
 
--- players table that keeps track for player's name and assigns them a unique id
+
+-- players table that keeps track for player's name 
+-- and assigns them a unique id
+drop table if exists players;
 create table players(
 	id serial primary key, 
 	name text
 	);
 
 -- matches table that keeps track of the wins and lose of the players
+drop table if exists matches;
 create table matches(
 	id serial primary key, 
 	winner integer references players (id), 
@@ -21,12 +25,14 @@ create table matches(
 	);
 
 -- view created to calculate number of wins of each player
+drop view if exists number_of_wins;
 create view number_of_wins as 
 	select players.id, count(matches.winner) as wins 
 	from players left join matches on players.id = matches.winner 
 	group by players.id;
 
 -- view created to calculate number of matches played by a player
+drop view if exists matches_played;
 create view matches_played as 
 	select players.id, count(matches.id) as number_of_matches 
 	from players left join matches on players.id = matches.winner or 
@@ -34,6 +40,7 @@ create view matches_played as
 	group by players.id;
 
 -- view created to get the players matches and wins.
+drop view if exists standings;
 create view standings as 
 	select players.id, players.name, number_of_wins.wins, 
 	matches_played.number_of_matches
